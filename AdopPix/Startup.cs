2c1d0d6a157cb0f472e85c2c778660265a78bd68
Colorfuls <94169899+mintcolorfuls@users.noreply.github.com>
2022-03-16 08:05:38 +0700

    init nuget
    initial nuget package for database mysql

--- AdopPix/Startup.cs
+++ AdopPix/Startup.cs
@@ -23,6 +23,7 @@ namespace AdopPix
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
             services.AddControllersWithViews();
         }
 
