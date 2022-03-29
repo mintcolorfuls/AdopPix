d93a94b3cef17a32f0dd82a57602ec9caa3c8ee9
Colorfuls <94169899+mintcolorfuls@users.noreply.github.com>
2022-03-29 10:20:08 +0700

    Delete old source code

--- AdopPix/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace AdopPix.Models
-{
-    public class User : IdentityUser
-    {
-    }
-}
--- AdopPix/Settings/AwsS3Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AdopPix.Settings
-{
-    public class AwsS3Configuration
-    {
-        public string AccessKey { get; set; }
-        public string SecretKey { get; set; }
-        public string BucketName { get; set; }
-    }
-}
