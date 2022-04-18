99d2bed8bf1cd8b39b7048d9c6e4b0788b9b94bd
Colorfuls <94169899+mintcolorfuls@users.noreply.github.com>
2022-04-18 23:21:02 +0700

    add interface EmailService

---
+++ AdopPix.Services/IServices/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AdopPix.Services.IServices
+{
+    public interface IEmailService
+    {
+        Task SendAsync(string from, string to, string subject, string body);
+    }
+}
