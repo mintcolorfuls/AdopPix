68d8996b10c439ae6a0096efddb33b0c53fe24d2
Colorfuls <94169899+mintcolorfuls@users.noreply.github.com>
2022-04-18 23:22:04 +0700

    initial EmailService

---
+++ AdopPix.Services/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using AdopPix.Services.IServices;
+using System.Threading.Tasks;
+
+namespace AdopPix.Services
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendAsync(string from, string to, string subject, string body)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
