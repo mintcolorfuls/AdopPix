5308f3fb62d6cc90f8dfcac216fa8c54ca2b8fe2
Colorfuls <94169899+mintcolorfuls@users.noreply.github.com>
2022-04-19 09:48:20 +0700

    add ConfirmEmail template

--- AdopPix.Services/EmailService.cs
+++ AdopPix.Services/EmailService.cs
@@ -1,13 +1,36 @@
 ﻿using AdopPix.Services.IServices;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace AdopPix.Services
 {
     public class EmailService : IEmailService
     {
-        public Task SendAsync(string from, string to, string subject, string body)
+        private readonly IConfiguration configuration;
+
+        public EmailService(IConfiguration configuration)
         {
-            throw new System.NotImplementedException();
+            this.configuration = configuration;
+        }
+        public async Task SendAsync(string from, string to, string subject, string body)
+        {
+            var massage = new MailMessage(from, to, subject, body);
+
+            massage.IsBodyHtml = true;
+
+            string host = configuration["SMTP_Host"];
+            string port = configuration["SMTP_Port"];
+            string user = configuration["SMTP_User"];
+            string password = configuration["SMTP_Password"];
+
+            using (var emailClient = new SmtpClient(host, Convert.ToInt32(port)))
+            {
+                emailClient.Credentials = new NetworkCredential(user, password);
+                await emailClient.SendMailAsync(massage);
+            }
         }
     }
 }
