3e948c8d341c02cd427616a38a7b63c0bdf57a3d
Colorfuls <94169899+mintcolorfuls@users.noreply.github.com>
2022-04-21 01:15:58 +0700

    add ResetPassword with out ui

--- AdopPix.Services/IServices/IEmailService.cs
+++ AdopPix.Services/IServices/IEmailService.cs
@@ -7,6 +7,7 @@ namespace AdopPix.Services.IServices
         string CreateTemplate(string templateType);
         string SetupConfirmEmailTemplate(string template, string url);
         string SetupChangeEmailTemplate(string template, string url);
+        string SetupForgetPasswordTemplate(string template, string url);
         Task SendAsync(string from, string to, string subject, string body);
     }
 }
