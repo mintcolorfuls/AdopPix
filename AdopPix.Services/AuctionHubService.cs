a38faaf5dc1a5884e6f490edb7a7bcb98890fcc1
Colorfuls <94169899+mintcolorfuls@users.noreply.github.com>
2022-05-16 12:50:26 +0700

    add NotificationByUserId with out process

--- AdopPix.Services/AuctionHubService.cs
+++ AdopPix.Services/AuctionHubService.cs
@@ -3,6 +3,7 @@ using AdopPix.Services.IServices;
 using AdopPix.Services.ModelService;
 using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AdopPix.Services
@@ -15,6 +16,12 @@ namespace AdopPix.Services
         {
             this.hubContext = hubContext;
         }
+
+        public Task NotificationByUserId(string from, IReadOnlyList<string> to, string auctionId, string description)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task UpdateClientsAsync(string auctionId, UpdateClientModelService model)
         {
             await hubContext.Clients.All.SendAsync(auctionId, new { avatarName = model.AvatarName,
